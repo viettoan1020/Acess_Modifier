78617098ba1045c1447c218e57de16fdd2012925
viettoan1020 <viettoann102003@gmail.com>
2024-01-22 16:08:45 +0700

    Access modifier

---
+++ areaCircle/src/Circle.java
@@ -0,0 +1,21 @@
+public class Circle {
+    private double radius;
+    private String color ;
+
+    public Circle() {
+        radius = 1.0;
+        color = "red";
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return radius*radius*Math.PI;
+    }
+}
---
+++ areaCircle/src/Main.java
@@ -0,0 +1,9 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Circle c = new Circle();
+        System.out.println("Radius : " + c.getRadius());
+        System.out.println("Area : " + c.getArea());
+    }
+}
