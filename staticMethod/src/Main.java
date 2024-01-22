2d4f5c89e984089b63b24cea2af71eb3d7701470
viettoan1020 <viettoann102003@gmail.com>
2024-01-22 14:00:21 +0700

    thuc hanh 1

---
+++ staticMethod/src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Student.change();
+
+        Student s1 = new Student(111, "Toan");
+        Student s2 = new Student(112, "Nhon");
+        Student s3 = new Student(113, "Dat");
+        s1.display();
+        s2.display();
+        s3.display();
+    }
+}
