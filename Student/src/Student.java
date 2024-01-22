3801fd1f9a2d1b0da4fe807da3e9bb0c0ecb0656
viettoan1020 <viettoann102003@gmail.com>
2024-01-22 16:27:13 +0700

    Student assignment

---
+++ Student/src/Student.java
@@ -0,0 +1,25 @@
+public class Student {
+    private String name;
+    private String classes;
+
+    public Student() {
+        name = "John";
+        classes = "C02";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getClasses() {
+        return classes;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+}
---
+++ Student/src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.setName("toan");
+        student1.setClasses("c11");
+        System.out.println(student1.getName());
+        System.out.println(student1.getClasses());
+    }
+}
